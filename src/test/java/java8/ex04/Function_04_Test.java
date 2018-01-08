34d6c7eecca281c16339755e7c5e186be0caf93d
hmerciol <li.henri@merciol.fr>
2018-01-08 15:45:06 +0100

    ex 4, tp 3

--- src/test/java/java8/ex04/Function_04_Test.java
+++ src/test/java/java8/ex04/Function_04_Test.java
@@ -30,7 +30,8 @@ public class Function_04_Test {
     // tag::adult[]
     // TODO Compléter la fonction
     // TODO AGE >=18
-    Predicate<Person> adult = null;
+    Predicate<Person> adult = person -> person.getAge() >= 18;
+    //done
     // end::adult[]
 
     @Test
@@ -39,7 +40,8 @@ public class Function_04_Test {
         List<Person> personList = Data.buildPersonList();
 
         // TODO invoquer la méthode filter pour que le test soit passant
-        List<Person> result = null;
+        List<Person> result = filter(personList, adult);
+        //done
 
         assert result.size() == 4;
 
@@ -65,7 +67,8 @@ public class Function_04_Test {
 
         // TODO invoquer la méthode filter pour que le test soit passant
         // TODO chaîner les prédicats adult, lastnameIsFrance et firstnameIsArmor avec la méthode and
-        List<Person> result = null;
+        List<Person> result = filter(filter(filter(personList, adult), lastnameIsFrance), firstnameIsArmor);
+        //done
 
         assert result.size() == 1;
         assert result.get(0).getFirstname().equals("Armor");
